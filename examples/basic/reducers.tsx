15eef70398c81358207e405545e93206a7c16ad2
Ivan Lagunovsky <ivan.lagunovsky@gmail.com>
2021-07-15 20:00:39 +0300

    #1 fixed example of reducer

--- examples/basic/reducers.tsx
+++ examples/basic/reducers.tsx
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import { connectRouter } from '@lagunovsky/redux-react-router'
+import { createRouterReducer } from '@lagunovsky/redux-react-router'
 import { browserHistory } from './history'
 
 export const rootReducer = combineReducers({
